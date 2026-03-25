f401d4c31f29146c51ea190e595ff754a768d413
Kris Krüg <feelmoreplants@gmail.com>
2026-03-25 00:08:21 -0700

    feat: implement enhanced digital composting module (CPT + Taxonomy)

--- inc/digital-composting.php
+++ inc/digital-composting.php
@@ -1,22 +1,28 @@
 <?php
 /**
  * Digital Composting Module
- * Registers Custom Post Type for Transcripts
+ * Registers Custom Post Type for Transcripts and Topics Taxonomy
  */
-function kk_register_transcript_cpt() {
-    $labels = array(
-        'name' => 'Transcripts',
-        'singular_name' => 'Transcript',
-        'menu_name' => 'Transcripts',
-    );
-    $args = array(
-        'labels' => $labels,
+function kk_register_transcript_assets() {
+    register_post_type('transcript', [
+        'labels' => [
+            'name' => 'Transcripts',
+            'singular_name' => 'Transcript',
+            'menu_name' => 'Transcripts',
+            'add_new' => 'Add New Transcript',
+        ],
         'public' => true,
         'has_archive' => true,
         'show_in_rest' => true,
         'menu_icon' => 'dashicons-media-text',
-        'supports' => array('title', 'editor', 'excerpt', 'custom-fields'),
-    );
-    register_post_type('transcript', $args);
+        'supports' => ['title', 'editor', 'excerpt', 'custom-fields'],
+        'taxonomies' => ['transcript_topic'],
+    ]);
+
+    register_taxonomy('transcript_topic', 'transcript', [
+        'labels' => ['name' => 'Transcript Topics', 'singular_name' => 'Topic'],
+        'hierarchical' => true,
+        'show_in_rest' => true,
+    ]);
 }
-add_action('init', 'kk_register_transcript_cpt');
+add_action('init', 'kk_register_transcript_assets');
